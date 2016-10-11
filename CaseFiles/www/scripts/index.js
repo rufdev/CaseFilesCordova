ab35b2fa897d1fbcfc1f2e0260684e4dda2fe02d
RUFINO <aguilarufino@gmail.com>
2016-10-11 15:05:50 +0800

    host case file app on cordova app

--- CaseFiles/www/scripts/index.js
+++ CaseFiles/www/scripts/index.js
@@ -9,8 +9,8 @@
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+        //document.addEventListener( 'pause', onPause.bind( this ), false );
+        //document.addEventListener( 'resume', onResume.bind( this ), false );
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         //var parentElement = document.getElementById('deviceready');
@@ -19,22 +19,26 @@
         //listeningElement.setAttribute('style', 'display:none;');
         //receivedElement.setAttribute('style', 'display:block;');
 
-        $('#get-cases').click(function () {
-            $("#case-list").show();
-            $.get("http://mangtaswebapi.dev/api/cases", function (data) {
-                var cases = [];
-                $.each(data.cases, function (k, v) {
-                    cases.push('<li data-role="list-divider" id="title" class="ui-li-divider ui-bar-a">'+v.caseno+'</li>');
-                    cases.push('<li>Case Title: <span>' + v.title + '</span></li>');
-                });
-                $("#case-list").html(cases.join(""));
+        //$('#get-cases').click(function () {
+        //    $("#case-list").show();
+        //    $.get("http://mangtaswebapi.dev/api/cases", function (data) {
+        //        var cases = [];
+        //        $.each(data.cases, function (k, v) {
+        //            cases.push('<li data-role="list-divider" id="title" class="ui-li-divider ui-bar-a">'+v.caseno+'</li>');
+        //            cases.push('<li>Case Title: <span>' + v.title + '</span></li>');
+        //        });
+        //        $("#case-list").html(cases.join(""));
                 
-            });
-            return false;
-        });
-
+        //    });
+        //    return false;
+        //});
+        //if (navigator.connection.type == Connection.NONE) {
+        //    navigator.notification.alert('An internet connection is required to continue');
+        //} else {
+            window.location = "http://mangtaswebapi-aguilarufino790764.codeanyapp.com";
+        //}
     };
-
+   
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
     };
